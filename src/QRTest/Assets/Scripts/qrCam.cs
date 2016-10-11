9a7ee6dadcdbc94ca5d384bd1fb55fd9228b2188
SHEePYTaGGeRNeP <neppy@live.nl>
2016-10-11 14:50:32 +0200

    Added ZXING BARCODE EXAMPLE. qrCAM kan al saven naar TExture2D.

--- src/QRTest/Assets/Scripts/qrCam.cs
+++ src/QRTest/Assets/Scripts/qrCam.cs
@@ -5,11 +5,16 @@ using UnityEngine.UI;
 using System.Threading;
 
 using com.google.zxing.qrcode;
+using System.Collections;
+using com.google.zxing.common;
 
 public class qrCam : MonoBehaviour
 {
 	[SerializeField]
 	private Text _text;
+
+    [SerializeField] private Texture2D _texture2D;
+
 	private Renderer _planeRenderer;
 
     private WebCamTexture camTexture;
@@ -51,6 +56,7 @@ public class qrCam : MonoBehaviour
 	}
     private void Start()
     {
+        this._texture2D = new Texture2D(250, 250);
         this.camTexture = new WebCamTexture();
 		this._planeRenderer.material.mainTexture = this.camTexture;
         this.OnEnable();
@@ -83,5 +89,68 @@ public class qrCam : MonoBehaviour
         catch
         { // ignore -> Alot of exceptions are thrown.
         }
+    }
+
+    public void WriteQR()
+    {
+        QRCodeWriter writer = new QRCodeWriter();
+        Hashtable hints = new Hashtable();
+
+        hints.Add(com.google.zxing.EncodeHintType.ERROR_CORRECTION, com.google.zxing.qrcode.decoder.ErrorCorrectionLevel.M);
+        hints.Add("Version", "7");
+        ByteMatrix byteIMGNew = writer.encode("Hello", com.google.zxing.BarcodeFormat.QR_CODE, 350, 350, hints);
+        sbyte[][] imgNew = byteIMGNew.Array;
+        int count = imgNew.Length;
+        int count2 = imgNew[0].Length;
+        Debug.Log(count + "  " + count2);
+
+        Texture2D tex = new Texture2D(350, 350);
+        Color[] colors = new Color[tex.width * tex.height];
+        for (int i = 0; i < imgNew.Length; i++)
+            for (int j =0 ; j < imgNew[i].Length; j++)
+        {
+            if (imgNew[i][j] == 0)
+                colors[i + j] = imgNew[i][j] == 0 ? Color.black : Color.white;
+        }
+        tex.SetPixels(colors);
+        tex.Apply();
+        
+
+        //Bitmap bmp1 = new Bitmap(byteIMGNew.Width, byteIMGNew.Height);
+        //Graphics g1 = Graphics.FromImage(bmp1);
+        //g1.Clear(Color.White);
+        //for (int i = 0; i <= imgNew.Length - 1; i++)
+        //{
+        //    for (int j = 0; j <= imgNew[i].Length - 1; j++)
+        //    {
+        //        if (imgNew[j][i] == 0)
+        //        {
+        //            g1.FillRectangle(Brushes.Black, i, j, 1, 1);
+        //        }
+        //        else
+        //        {
+        //            g1.FillRectangle(Brushes.White, i, j, 1, 1);
+        //        }
+        //    }
+        //}
+        //bmp1.Save("D:\\QREncode.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+    }
+
+
+    private void UpdateTexture()
+    {
+        //var writer = new BarcodeWriter
+        //{
+        //    Format = BarcodeFormat.QR_CODE,
+        //    Options = new QrCodeEncodingOptions
+        //    {
+        //        Height = height,
+        //        Width = width
+        //    }
+        //};
+        //Color32 color = writer.Write(textForEncoding);
+        QRCodeWriter writer2 = new QRCodeWriter();
+        com.google.zxing.common.ByteMatrix matrix = writer2.encode("hey", com.google.zxing.BarcodeFormat.QR_CODE, this._texture2D.width, this._texture2D.height);
+        
     }
 }
