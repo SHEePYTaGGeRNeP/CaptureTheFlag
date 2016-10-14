8e25ab935232fe8bf78810f4a012dc5d83a93e8a
SHEePYTaGGeRNeP <neppy@live.nl>
2016-10-14 14:51:40 +0200

    Unity NFC test

---
+++ src/unity-nfc-plugin-master/Assets/Plugins/AndroidNFCReader.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class AndroidNFCReader : MonoBehaviour
+{
+    public const string ERROR = "ERROR";
+    public const string NO_HARDWARE = "NO_HARDWARE";
+    public const string CANCELLED = "CANCELLED";
+    public const string NO_ALLOWED_OS = "NO_ALLOWED_OS";
+    
+
+    public static void enableBackgroundScan()
+    {
+#if UNITY_ANDROID && !UNITY_EDITOR
+		AndroidJavaClass javaClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"); 
+		AndroidJavaObject javaObject = javaClass.GetStatic<AndroidJavaObject>("currentActivity"); 
+		javaObject.Call("enableBackgroundScan");
+#endif
+    }
+
+    public static void disableBackgroundScan()
+    {
+#if UNITY_ANDROID && !UNITY_EDITOR
+		AndroidJavaClass javaClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"); 
+		AndroidJavaObject javaObject = javaClass.GetStatic<AndroidJavaObject>("currentActivity"); 
+		javaObject.Call("disableBackgroundScan");
+#endif
+    }
+
+
+    public static void ScanNFC(string objectName, string functionName)
+    {
+        switch (Application.platform)
+        {
+            case RuntimePlatform.Android:
+                AndroidJavaClass javaClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+                AndroidJavaObject javaObject = javaClass.GetStatic<AndroidJavaObject>("currentActivity");
+                javaObject.Call("scanNFC", objectName, functionName);
+                break;
+            default:
+                GameObject.Find(objectName).SendMessage(functionName, NO_ALLOWED_OS);
+                break;
+        }
+    }
+}
---
+++ src/unity-nfc-plugin-master/Assets/Scripts/Main.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    [SerializeField] private UnityEngine.UI.Text text;
+	string qrString = "";
+	bool background;
+
+	void Start(){
+		//AndroidNFCReader.enableBackgroundScan ();
+		//AndroidNFCReader.ScanNFC (gameObject.name, "OnFinishScan");
+	}
+
+	void OnGUI ()
+	{
+		if (!background) {
+			// Scan NFC button
+			if (GUI.Button (new Rect (0, Screen.height - 50, Screen.width, 50), "Scan NFC")) {
+				AndroidNFCReader.ScanNFC (gameObject.name, "OnFinishScan");
+			}
+			if (GUI.Button (new Rect (0, Screen.height - 100, Screen.width, 50), "Enable Backgraound Mode")) {
+				AndroidNFCReader.enableBackgroundScan ();
+				background = true;
+			}
+		}else{
+			if (GUI.Button (new Rect (0, Screen.height - 50, Screen.width, 50), "Disable Backgraound Mode")) {
+				AndroidNFCReader.disableBackgroundScan ();
+				background = false;
+			}
+		}
+		GUI.Label (new Rect (0, 0, Screen.width, 50), "Result: " + qrString);
+	}
+
+	// NFC callback
+	void OnFinishScan (string result)
+	{
+	    try
+	    {
+            switch (result)
+            {
+                case AndroidNFCReader.CANCELLED:
+                case AndroidNFCReader.ERROR:
+                case AndroidNFCReader.NO_HARDWARE:
+                case AndroidNFCReader.NO_ALLOWED_OS:
+                    text.text = result;
+                    break;
+            }
+
+	        qrString = getToyxFromUrl(result);
+	        text.text = qrString;
+	    }
+        catch (System.Exception ex)
+        {
+            text.text = "ex : " + ex.Message;
+        }
+	}
+
+	// Extract toyxId from url
+	string getToyxFromUrl (string url)
+	{		
+		int index = url.LastIndexOf ('/') + 1;
+		
+		if (url.Length > index) {
+			return url.Substring (index);		
+		} 
+		
+		return url;
+	}
+
+}
